45196a6e2b8e4b494f7a818d8e3f084e7040aaec
lamiix <lamiizemail@gmail.com>
2023-08-30 16:29:24 +0530

    UP

--- web/urls.py
+++ web/urls.py
@@ -6,6 +6,7 @@ app_name = "web"
 
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
+    path('about', views.AboutView.as_view(), name='about'),
     path('MyResume', views.ResumeView.as_view(), name='resume'),
     path('MyPortfolio', views.PortfolioView.as_view(), name='portfolio'),
     path('contact-me', views.ContactView.as_view(), name='contact'),
--- web/views.py
+++ web/views.py
@@ -7,7 +7,13 @@ class IndexView(TemplateView):
         context = super().get_context_data(**kwargs)
         context["is_index"] = True
         return context
-    
+
+class AboutView(TemplateView):
+    template_name = "web/about.html"
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["is_about"] = True
+        return context
 
 class ResumeView(TemplateView):
     template_name = "web/resume.html"

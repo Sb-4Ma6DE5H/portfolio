615fcecd078da5b22a71a1e416eda7e6b341d793
scorpio <124780230+lamiiiizee@users.noreply.github.com>
2023-09-02 18:02:09 +0530

    migrate ad web

--- build_files.sh
+++ build_files.sh
@@ -3,7 +3,7 @@
 echo "Installing requirements..."
 python3.9 -m pip install -r requirements.txt
 echo "makemigrations..."
-python3.9 manage.py makemigrations --noinput
-python3.9 manage.py migrate --noinput
+python3.9 manage.py makemigrations web --noinput
+python3.9 manage.py migrate web --noinput
 echo "collect static..."
 python3.9 manage.py collectstatic

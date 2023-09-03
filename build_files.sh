37b4c20140160c915c225b0b8d4e71283e19a746
scorpio <muhammedlameesmv@gmail.com>
2023-09-03 14:51:16 +0530

    drop db create new db

--- build_files.sh
+++ build_files.sh
@@ -4,6 +4,8 @@ echo "Installing requirements...💃"
 python3.9 -m pip install -r requirements.txt
 echo "makemigrations...💃"
 python3.9 manage.py makemigrations web --noinput
+echo "showmigrations...💃"
+python manage.py showmigrations
 echo "migrate...💃"
 python3.9 manage.py migrate --noinput
 echo "collect static...💃"

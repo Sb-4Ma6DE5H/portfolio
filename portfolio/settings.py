2d8d6dcf81dd0179707e904672b6562bb53f979c
scorpio <muhammedlameesmv@gmail.com>
2023-09-03 16:22:18 +0530

    chmod -R 755 media

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -144,7 +144,8 @@ DATETIME_INPUT_FORMATS = (
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 MEDIA_URL = "/media/"
-MEDIA_ROOT = BASE_DIR / "media"
+# MEDIA_ROOT = BASE_DIR / "media"
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATIC_URL = "/static/"
 STATIC_FILE_ROOT = BASE_DIR / "static"
 STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
